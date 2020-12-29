e55681c37fd27376cf97baa1e7350d48d59b27ec
BBaoVanC <github@bbaovanc.com>
2020-12-29 14:40:41 -0600

    Temporarily disable some autostart items

--- autostart.sh
+++ autostart.sh
@@ -2,10 +2,10 @@
 xset r rate 300 50  # keyboard repeat speed
 feh --no-fehbg --bg-fill ~/wallpaper  # set background
 
-nm-applet &  # networkmanager tray icon
+#nm-applet &  # networkmanager tray icon
 picom -b  # compositor (daemonized)
 light-locker &  # enable lightdm locking
 
-echo UPDATESTARTUPTTY | gpg-connect-agent &
+#echo UPDATESTARTUPTTY | gpg-connect-agent &
 
 systemctl --user restart redshift &
