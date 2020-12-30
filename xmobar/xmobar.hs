442e50a08cfc7ab9a3a1822d573f15c44d6a06c6
BBaoVanC <github@bbaovanc.com>
2020-12-29 20:13:08 -0600

    Finish right side of bar

--- xmobar/xmobar.hs
+++ xmobar/xmobar.hs
@@ -16,12 +16,15 @@ Config { font = "xft:JetBrainsMono Nerd Font:style=Bold:size=10:antialias=true:h
        , allDesktops = True
        , overrideRedirect = True
        , commands = [
-                      Run Cpu ["-t"," <total>%"] 20
-                  --, Run Memory ["-t"," <used>M (<cache>M)"] 20
+                      Run Cpu ["-t","<total>%"] 20
+                    , Run Memory ["-t","<usedratio>%"] 20
+                    , Run Kbd []
+                    , Run Alsa "default" "Master" ["-t", "<status><volume>%", "--", "-o", "<fc=#d8dee9>婢 </fc>", "-O", "<fc=#d8dee9>墳 </fc>"]
+                    -- 婢奄奔墳
                     , Run Date "%a %F %r" "date" 10
                     ]
        , sepChar = "%"
        , alignSep = "}{"
        , template = "%UnsafeStdinReader% }\
-                    \{ %cpu% | %keymap.block% | %volume.block% | %date%"
+                    \{  %cpu% |  %memory% |  %kbd% | <action=`st -c st-floating -e pulsemixer` button=1>%alsa:default:Master%</action> |  %date%"
        }
