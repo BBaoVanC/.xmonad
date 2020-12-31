0f14a885358571494b1e5216b200efaa38d5f950
BBaoVanC <github@bbaovanc.com>
2020-12-30 22:47:33 -0600

    Add color to xmobar

--- xmobar/xmobar.hs
+++ xmobar/xmobar.hs
@@ -12,7 +12,7 @@ Config { font = "xft:JetBrainsMono Nerd Font:style=Bold:size=10:antialias=true:h
                       Run Cpu ["-t", "<total>%"] 20
                     , Run Memory ["-t", "<usedratio>%"] 20
                     , Run Kbd []
-                    , Run Alsa "default" "Master" ["-t", "<status><volume>%", "--", "-o", "<fc=#d8dee9>婢 </fc>", "-O", "<fc=#d8dee9>墳 </fc>"]
+                    , Run Alsa "default" "Master" ["-t", "<status><volume>%", "--", "-o", "<fc=#88c0d0>婢 </fc>", "-O", "<fc=#88c0d0>墳 </fc>"]
                     -- 婢奄奔墳
                     , Run Date "%a %F %r" "date" 10
                     , Run UnsafeStdinReader
@@ -20,9 +20,15 @@ Config { font = "xft:JetBrainsMono Nerd Font:style=Bold:size=10:antialias=true:h
        , sepChar = "%"
        , alignSep = "}{"
        , template = "%UnsafeStdinReader% }\
-                    \{  %cpu% | \
-                    \ %memory% | \
-                    \ %kbd% | \
-                    \<action=`st -c st-floating -e pulsemixer` button=1>%alsa:default:Master%</action> | \
-                    \ %date%"
+                    \{ <fc=#bf616a>\
+                    \ %cpu% \
+                    \</fc><fc=#434c5e>|</fc> <fc=#ebcb8b>\
+                    \ %memory% \
+                    \</fc><fc=#434c5e>|</fc> <fc=#a3be8c>\
+                    \ %kbd% \
+                    \</fc><fc=#434c5e>|</fc> <fc=#88c0d0>\
+                    \<action=`st -c st-floating -e pulsemixer` button=1>%alsa:default:Master%</action> \
+                    \</fc><fc=#434c5e>|</fc> <fc=#b48ead>\
+                    \ %date% \
+                    \</fc>"
        }
